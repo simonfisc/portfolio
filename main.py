79adb09368690ffcd7009eb10ad5c70b3350b732
simonfisc <simon@mfisc.org>
2024-09-20 11:45:47 -0400

    Day 21: Add description on the new columns

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
+
 with col1:
     st.image('images/photo.png')
 
@@ -13,3 +15,8 @@ with col2:
     I have worked with companies from various contries.
     """
     st.info(content)
+
+content2 = ("""
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+""")
+st.text(content2)
