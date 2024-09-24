44dcfb13a4ce35a1ae06a4b347128217759af47c
simonfisc <simon@mfisc.org>
2024-09-24 00:20:20 -0400

    Day 24: store password securely

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message, from_email):
     host = 'smtp.gmail.com'
     port = 465
 
     username = "scarrier99@gmail.com"
-    password = "iqah exep rcws prxv"
+    password = os.getenv("GMAIL_PASSWORD")
 
     receiver = "scarrier99@gmail.com"
     context = ssl.create_default_context()
